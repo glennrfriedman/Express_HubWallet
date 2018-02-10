8b2f6b56d2e28585462a44ae36eb6af63ee9cb3d
Glenn Friedman <glennfriedman@Glenns-MBP.fios-router.home>
2018-02-10 13:31:29 -0500

    added edit route att /api/:coin_id/edit

--- hubwallet_db/controllers/coins.js
+++ hubwallet_db/controllers/coins.js
@@ -1,35 +1,6 @@
 const router = require('express').Router();
-
 // const auth = require('../services/auth');
-const Coins = require('../models/coins')
-// NEXT STEPS 
-// RETURN SAVED COIN DATA WITH PORTFOLIO CALC 
-// 1 - create schema and coin db -- DONE 
-// 2 - seed coin db with dummy coin data -- DONE 
-// 3 - make calls to db to return saved coin data in postman -- DONE 
-// 4 - add middleware to call coinmarketcap api using saved coin data -- DONE
-// 				 - return current coin information -- DONE
-// 					- calculate portfolio information -- TBD on FRONT END - think its best place to perform
-// 5 - render json of current price information and calculated portfolio information @ /api/coins/:id 
-// SEARCH -- THIS IS GOING TO BE TRICK since no search functionality in CMC API -- DONE IN BACK END with params /search/:searchTerm
-// create route for @ coins/search/:searchTerm which will return list of searched coins 
-// POST -- DONE
-// create route for @ coins/coinId which will save coin info (name, id, price, shares, getDateTime)
-// DELETE -- DONE
-// delete a coin from a users account @ /coins/delete
-// EDIT 
-// add an edit route where users can update price, shares or add another lot
-// ADDED - NEED TO ADD .env file with global varaiables along with .gitignore file
-// ADDING AUTH
-// middleware currently - auth.restrict - replace this with example using auth0 in react-authenticaotion-tutorial
-// the Auth.js file in (../services/auth) will use that function instead (partially completed)
-// this auth will restrict users unless logged in and return only coins saved to specific users
-// auth middleware will be added to all routes GET, POST, EDIT and DELETE
-// REACT
-// need to add login/logout button 
-// add utils folder files and configure 
-// AUTH0
-// need to go in and configure api for Hub Wallet same was as Chuck Norris API
+const Coins = require('../models/coins');
 
 router.get('/search/:searchTerm',
     // auth.authCheck, // restrict this route to logged in users
@@ -53,6 +24,13 @@ router.post('/save',
 		(req, res) => {
 			res.json({ newCoin: res.locals.newCoinData });
 		});
+
+router.post('/:coin_id/edit/',
+        // auth.authCheck, // restrict this route to logged in users
+        Coins.update,
+        (req, res) => {
+            res.json({ editedCoin: res.locals.editedCoinData });
+        });
 
 router.delete('/coins/:coin_id',
     // auth.authCheck, // restrict this route to logged in users
--- hubwallet_db/models/coins.js
+++ hubwallet_db/models/coins.js
@@ -1,23 +1,13 @@
 const axios = require('axios');
 const query = require('array-query');
 const db = require('../db/config');
-// const convertTime = require('convert-time');
-// const dateformatConverter = require('dateformat-converter');
-// const dateFormatLite = require("date-format-lite");
-// require('dotenv').config();
 
 // need to add /COIN_ID//?convert=USD
 const API_URL = 'https://api.coinmarketcap.com/v1/ticker';
 
 const Coins = {};
 
-    // edit middleware 
-    // need to have 2 edit routes 
-    // 1 - Edit route where users can edit investment information porovided 
-    // 2 - Edit route where I can insert new coin data from api into the database for each coin 
-    //      - this way when I make call from front-end savedCoin data will return the live information as well 
-    //      - much easier to present information and do calc if all coin data in same object - avoid trying to implement something like MongoDb
-
+    // search CMC API based on user input
 	Coins.search = (req, res, next) => {
         const searchTerm = req.params.searchTerm.toLowerCase();
         axios.get(`${API_URL}/?limit=0`)
@@ -33,6 +23,7 @@ const Coins = {};
             }).catch(err => console.log('ERROR IN Coins.search:', err))
     },
 
+    // get all saved coin information for user 
     Coins.findAllForUser = (req, res, next) => {
         // this will need to change to either come from body or params like so 
         // const userId = req.user.id;
@@ -45,6 +36,7 @@ const Coins = {};
         }).catch(err => console.log('ERROR IN Coins.findAll:', err))
     },
 
+    // get all market data for saved coins and add to saved coins JSON 
     Coins.getMarketData = (req, res, next) => {
         const userId = req.params.id;
         // this will need to be axios.all call
@@ -61,20 +53,20 @@ const Coins = {};
             .then(response => {
                 // OLD LOGIC - returrning new key value with current coin data
                 let currentCoinData = [];
-                // console.log('response from axios.all is', response)
+                console.log('response from axios.all is', response)
                 response.forEach(function(response) {
                     currentCoinData.push(response.data);
                 })
                 currentCoinData.forEach(function(response) {
-                console.log('response from currentCoinData is ', response[0]);
-                let { price_usd, price_btc, market_cap_usd, percent_change_1h, percent_change_24h, percent_change_7d } = response[0];
+                    // console.log('response from currentCoinData is ', response[0]);
+                    let { price_usd, price_btc, market_cap_usd, percent_change_1h, percent_change_24h, percent_change_7d } = response[0];
                         // res.locals.savedCoinData[i].price_usd = 0;
                         // console.log('response in for loop is ', response.data[0].price_usd);
-                console.log('length of res.locals.savedCoinData is', res.locals.savedCoinData.length);
-                for(var i = 0; i < res.locals.savedCoinData.length; i++){
+                    // console.log('length of res.locals.savedCoinData is', res.locals.savedCoinData.length);
+                    for(var i = 0; i < res.locals.savedCoinData.length; i++){
                     // console.log('coin id from res.locals is', res.locals.savedCoinData[i].coin_id);
                     // console.log('coin id from response is', response[0].id);
-                    if (res.locals.savedCoinData[i].coin_id === response[0].id){
+                        if (res.locals.savedCoinData[i].coin_id === response[0].id){
                             res.locals.savedCoinData[i].price_usd = price_usd;
                             res.locals.savedCoinData[i].price_btc = price_btc;
                             res.locals.savedCoinData[i].market_cap_usd = market_cap_usd;
@@ -89,9 +81,10 @@ const Coins = {};
                     }
                 })
                 next();
-            })
+            }).catch(err => console.log('ERROR IN Coins.getMarketData:', err))
     },
 
+    // calculate user portfolio data based on current market data 
     Coins.portfolioData = (req, res, next) => {
         res.locals.portfolio = { total_investment: 0, total_npv: 0, total_roi_dollars: 0, total_roi_percent: 0 }
         res.locals.savedCoinData.forEach(function(coin) {
@@ -103,6 +96,7 @@ const Coins = {};
         next();
     },
 
+    // allow users to save coin
     Coins.saveCoin = (req, res, next) => {
         // need to change to req.user.id once implemented in React
         const user_id = req.body.user_id,
@@ -124,6 +118,22 @@ const Coins = {};
             })
     },
 
+    Coins.update = (req, res, next) => {
+        const coin_id = req.params.coin_id;
+        const { investment, shares, date_of_transaction } = req.body
+        const price_per_share = investment/shares;
+        db.one(`UPDATE coins
+                SET investment = $1, shares = $2, date_of_transaction = $3, price_per_share = $4
+                WHERE id = $5 returning *`, [ investment, shares, date_of_transaction, price_per_share, coin_id ])
+        .then((editedCoinData) => {
+            res.locals.editedCoinData = editedCoinData;
+            next();
+        }).catch(err => {
+                console.log('error in update coin is', err);
+            })
+    },
+
+    // allow users to delete coins
     Coins.destroy = (req, res, next) => {
         const coin_id = req.params.coin_id;
         db.none(
